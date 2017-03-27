a0c5a94734d2494d0dd85f0a968f3e8ebe98fad6
Minh Dong <minhdn1210@gmail.com>
2017-03-27 15:29:57 -0700

    add new feature for bot

--- server/controllers/bot.js
+++ server/controllers/bot.js
@@ -14,10 +14,48 @@ function init(app) {
   const bot = new builder.UniversalBot(connector);
   app.post('/api/messages', connector.listen());
 
-  // bot dialog
-  bot.dialog('/', (session) => {
-    session.send('Hi there');
-  });
+  // Start bot by guessing user intents from first command
+  const intents = new builder.IntentDialog();
+  bot.dialog('/', intents);
+
+  // If user want to change name
+  intents.matches(/^change name/i, [
+    function (session) {
+      session.beginDialog('/profile');
+    },
+    function (session, results) {
+      if (results.response) {
+        session.send('Ok... Changed your name to %s', session.userData.userName);
+      } else {
+        session.send('Hmm... Couldn\'t change your name to %s', session.userData.userName);
+      }
+    },
+  ]);
+
+  intents.onDefault([
+    function (session, args, next) {
+      if (!session.userData.name) {
+        session.beginDialog('/profile');
+      } else {
+        next();
+      }
+    },
+    function (session, results) {
+      session.send('Hello %s!', session.userData.userName);
+    },
+  ]);
+
+  // deal with user information
+  bot.dialog('/profile', [
+    function (session) {
+      builder.Prompts.text(session, 'whats your name?');
+    },
+    function (session, results) {
+      const currentSession = session;
+      currentSession.userData.userName = results.response;
+      session.endDialog();
+    },
+  ]);
 }
 
 module.exports.init = init;
